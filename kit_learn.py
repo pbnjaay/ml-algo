f3a2256f3e9a984c9a32b1cb8f6fa53f4f065619
Papa Basirou ndiaye <pbnlgk@gmail.com>
2023-04-10 05:11:40 +0000

    Linear regression model

--- kit_learn.py
+++ kit_learn.py
@@ -1,4 +1,33 @@
+import numpy as np
+
 class LinearRegression:
+    """
+    A simple implementation of linear regression using gradient descent.
+
+    Parameters
+    ----------
+    alpha : float, optional
+        The learning rate of the gradient descent algorithm. Default is 0.001.
+    nb_iterations : int, optional
+        The number of iterations to run the gradient descent algorithm. Default is 1000.
+
+    Attributes
+    ----------
+    w : ndarray of shape (n_features,)
+        The coefficients of the linear regression model.
+    b : float
+        The intercept of the linear regression model.
+
+    Methods
+    -------
+    fit(X, y)
+        Fit the linear regression model to the training data.
+    predict(X)
+        Predict the output of the linear regression model on new data.
+    zscore_normalize_features(X)
+        Normalize the input features of X using the z-score normalization method.
+
+    """
     def __init__(self, alpha = 0.001, nb_iterations=1000) -> None:
         self.alpha = alpha
         self.nb_iterations = nb_iterations
@@ -6,23 +35,65 @@ class LinearRegression:
         self.b = None
 
     def fit(self, X, y) -> None:
-        m = len(X)
-        n = len(X[0])
-        self.w = [0]*m
+        """
+        Fits the LinearRegression model.
+
+        Args:
+          X (ndarray (m,n)): input data, m examples, n features
+          y (ndarray (m,)): target variable
+
+        Returns:
+          None
+        """
+        m = X.shape[1]
+        self.w = np.array([0]*m)
         self.b = 0
 
         for k in range(self.nb_iterations):
-            y_hat = [sum([((self.w[i] * X[i][l]) + self.b) for l in range(n)]) for i in range(m)]
+            y_pred = np.dot(X, self.w) + self.b
 
-            e = [y_hat[i] - y[i] for i in range(m)]
-            
+            residue = y_pred - y
+
             for i in range(m):
-                    self.w[i] -= self.alpha * 1/m * sum([e[i]* X[i][l] for l in range(n)])
+                    self.w[i] -= self.alpha * 1/m * np.sum(np.dot(residue, X))
 
-            self.b -= self.alpha * 1/m  * sum(e)
-
-            e_square = [e[i]**2 for i in range(m)]
-            J = (1/(2*m)) * sum(e_square)
-
+            self.b -= self.alpha * 1/m  * np.sum(residue)
+            
+            # J = (1/(2*m)) * np.sum(np.square(residue))
+            
+            # if k % 100 == 0:
+            #     print(k, ' cost: ', J)
+                
     def predict(self, x)-> list:
-        return [(sum([self.w[l] * x[i][l] for l in range(len(x[0]))]) + self.b) for i in range(len(x))]
+        """
+        Predicts the target variable.
+
+        Args:
+          x (ndarray (m,n)): input data, m examples, n features
+
+        Returns:
+          result (list): predicted target variable for each example
+        """
+        result = []
+        for i in range(x.shape[0]):
+            result.append(np.sum(np.dot(self.w, x[i])) + self.b)
+
+        return result
+    
+    def zscore_normalize_features(self, X):
+        """
+        computes  X, zcore normalized by column
+
+        Args:
+          X (ndarray (m,n))     : input data, m examples, n features
+
+        Returns:
+          X_norm (ndarray (m,n)): input normalized by column
+          mu (ndarray (n,))     : mean of each feature
+          sigma (ndarray (n,))  : standard deviation of each feature
+        """
+        mu = np.mean(X, axis=0)                 
+        sigma = np.std(X, axis=0)          
+        X_norm = (X - mu) / sigma      
+
+        return X_norm
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from kit_learn import LinearRegression
+import numpy as np
 
 model = LinearRegression()
 
-X = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]] 
-
-y = [4, 7, 10, 13]
+X = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
+y = np.array([4, 7, 10, 13])
 
 model.fit(X, y)
-print(model.predict([[2, 3, 4], [5, 6, 7]]))
+
+X_new = np.array([[2, 3, 4], [5, 6, 7]])
+p = model.predict(X_new)
+
+print(p)
